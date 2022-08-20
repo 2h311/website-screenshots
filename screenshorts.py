e866f6d08dc9fa4d2812b332aca73e3b50efe231
2h311 <kcodepal@outlook.com>
2022-08-20 20:36:45 +0100

    Script is usable, onto creating the django app

--- screenshorts.py
+++ screenshorts.py
@@ -1,10 +1,11 @@
-'''
+"""
 TODO: write a bash script to automate this
 /usr/bin/google-chrome --remote-debugging-port=9222 --new-window https://google.com --user-data-dir=./selenium/ChromeProfile
-'''
+"""
 
 import re
 import time
+import pprint
 from pathlib import Path
 
 from Screenshot import Screenshot
@@ -12,54 +13,63 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-images_folder = Path(Path.cwd(), 'images')
+images_folder = Path(Path.cwd(), "images")
 if not images_folder.exists():
-	images_folder.mkdir()
+    images_folder.mkdir()
 
 
 def get_file_content(file: str):
-	filename = file if file else input("\aEnter a valid filename: ")
-	
-	if (path_object := Path(filename)).exists():
-		with path_object.open() as file_handler:
-			content = [ line.strip() for line in file_handler.readlines() ]
-	else:
-		content = list()
+    filename = file if file else input("\aEnter a valid filename: ")
 
-	return content	
+    if (path_object := Path(filename)).exists():
+        with path_object.open() as file_handler:
+            content = [line.strip() for line in file_handler.readlines()]
+    else:
+        content = list()
+
+    return content
 
 
 def get_chromedriver_object():
-	chrome_options = Options()
-	# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-	chromedriver_path = Path('driver') / Path('chromedriver')
-	return webdriver.Chrome(service=Service(chromedriver_path), options=chrome_options)
+    chrome_options = Options()
+    # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    chromedriver_path = Path("driver") / Path("chromedriver")
+    return webdriver.Chrome(service=Service(chromedriver_path), options=chrome_options)
 
 
-# read in urls from txt file
-urls = get_file_content('urls.txt')
-url = urls[5]
+def get_screenshot(url: str):
+    driver.get(url)
 
-driver = get_chromedriver_object()
-driver.get(url)
+    timestamp = time.time()
+    domain = re.search("(\w{3}.)(\w+.\w+)", url).groups()[1]
+    screenshot_name = f"{domain}-{timestamp}.png"
 
-# def make_filename_with_timestamp(url: str, separator: str = '|'):
+    screenshot_object = Screenshot.Screenshot()
+    image_url = screenshot_object.full_Screenshot(
+        driver,
+        save_path=images_folder.name,
+        image_name=screenshot_name,
+        multi_images=True,
+    )
+    return {
+        "title_of_the_website": driver.title,
+        "screenshot_url": image_url,
+        "timestamp": timestamp,
+        "website_url": url,
+    }
 
-separator: str = '|'
-timestamp = time.time()
-domain = re.search('(\w{3}.)(\w+.\w+)', url).groups()[1]
-screenshot_name = f"{domain}{separator}{timestamp}.png"
-dict_ = {
-	'title_of_the_website': driver.title,
-	'screenshot_url': screenshot_name,
-	'timestamp': timestamp,
-	'website_url': url,
-}
-print(dict_)
+
+def main():
+    # read in urls from txt file
+    urls = get_file_content("urls.txt")
+    for url in urls:
+        dict_ = get_screenshot(url)
+        pprint.pprint(dict_)
+        time.sleep(1.2)
 
 
-
-# screenshot_object = Screenshot.Screenshot()
-# img_url = screenshot_object.full_Screenshot(driver, save_path=images_folder.name, image_name='Myimage.png', multi_images=True)
-
-# print(img_url)
+if __name__ == "__main__":
+    driver = get_chromedriver_object()
+    driver.implicitly_wait(45)
+    main()
+    driver.quit()
--- test_main.py
+++ test_main.py
@@ -2,26 +2,27 @@ from selenium import webdriver
 
 from screenshorts import get_file_content, get_chromedriver_object
 
+
 def test_user_receives_a_list_for_reading_file_content():
-	filename = 'urls.txt'
-	return_value = get_file_content(filename)
-	assert isinstance(return_value, list)
+    filename = "urls.txt"
+    return_value = get_file_content(filename)
+    assert isinstance(return_value, list)
 
 
 def test_returns_a_non_empty_list_for_a_correct_filename():
-	filename = 'urls.txt'
-	return_value = get_file_content(filename)
-	assert isinstance(return_value, list)
-	assert bool(return_value) == True
+    filename = "urls.txt"
+    return_value = get_file_content(filename)
+    assert isinstance(return_value, list)
+    assert bool(return_value) == True
 
 
 def test_returns_an_empty_list_for_an_incorrect_filename():
-	filename = 'crymeariver.csv'
-	return_value = get_file_content(filename)
-	assert isinstance(return_value, list)
-	assert bool(return_value) == False
+    filename = "crymeariver.csv"
+    return_value = get_file_content(filename)
+    assert isinstance(return_value, list)
+    assert bool(return_value) == False
 
 
 def test_creates_a_chromedriver_instance():
-	driver = get_chromedriver_object()
-	assert isinstance(driver, webdriver.Chrome)
+    driver = get_chromedriver_object()
+    assert isinstance(driver, webdriver.Chrome)
